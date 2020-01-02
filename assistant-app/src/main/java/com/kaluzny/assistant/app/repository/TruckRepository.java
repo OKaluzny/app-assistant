96969e031b1a76aaa452a134f0eb11ff6e282bb4
Oleg Kaluzny <cryoengineer@gmail.com>
2020-01-02 17:23:20 +0200

    fixed repo

--- assistant-app/src/main/java/com/kaluzny/assistant/app/repository/TruckRepository.java
+++ assistant-app/src/main/java/com/kaluzny/assistant/app/repository/TruckRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
+ * Repository for {@link Truck}.
+ *
  * @author Oleg Kaluzny
  */
 @Repository

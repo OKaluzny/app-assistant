7e8fd8ad116bc49233859d83d7acffdd99f41685
Oleg Kaluzny <cryoengineer@gmail.com>
2020-02-18 18:05:37 +0200

    added address dto

---
+++ assistant-api/src/main/java/com/kaluzny/assistant/api/model/dto/AddressDto.java
@@ -0,0 +1,40 @@
+package com.kaluzny.assistant.api.model.dto;
+
+import com.kaluzny.assistant.api.utils.DiffBuilderUtils;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+import org.apache.commons.lang3.builder.DiffResult;
+import org.apache.commons.lang3.builder.Diffable;
+
+/**
+ * Model of Address
+ *
+ * @author Oleg Kaluzny
+ */
+@Getter
+@Setter
+@ToString
+@Accessors(chain = true)
+public class AddressDto implements Diffable<AddressDto> {
+
+    @ApiModelProperty(value = "Unique identifier a Address.", example = "1")
+    private Long id;
+
+    @ApiModelProperty(value = "country of an employee.", example = "Ukraine")
+    private String country;
+
+    @ApiModelProperty(value = "city of an employee.", example = "Odessa")
+    private String city;
+
+    @ApiModelProperty(value = "street of an employee.", example = "French Boulevard")
+    private String street;
+
+    @Override
+    public DiffResult diff(AddressDto obj) {
+        return DiffBuilderUtils.createBuilder(this, obj);
+    }
+}
